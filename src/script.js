bc67f46f2a2d8393a3b54711697a88d6babf1e01
YoungseopLee <dldudtjqlol@gmail.com>
2023-08-31 16:56:22 +0900

    "learn basic js and control browser console"

--- src/script.js
+++ src/script.js
@@ -1,1 +1,21 @@
 alert("Hello World!")
+
+const numInteger = 10; // int 
+const numFloat = 10.123 // float
+const str = "Hi :)" // string
+
+console.log(numInteger, numFloat, str);
+
+const numBoolean = true // bool
+console.log(numBoolean);
+
+const nnuull = null; // null 
+// const undi = ; // undefined
+console.log(nnuull);
+
+const arr = ["H", "E", "L", "L", "O"];
+console.log(arr[0]);
+arr.push(" :) ");
+console.log(arr)
+
+
